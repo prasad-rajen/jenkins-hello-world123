b936823669ff9271b86296e7de7523f7e801d5c6
Rajen Prasad <rajenprasad1990@gmail.com>
2026-09-03 14:16:34 +0000

    code change 2

--- src/test/java/com/kodekloud/hello_demo/HelloControllerTests.java
+++ src/test/java/com/kodekloud/hello_demo/HelloControllerTests.java
@@ -81,7 +81,7 @@ public class HelloControllerTests {
         )
         .andExpect(status().isOk())
         .andExpect(
-                content().string(startsWith("Hello"))
+                content().string(startsWith("Hola"))
         );
     }
 
